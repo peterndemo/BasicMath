4f6cbb388843bc33e22e5dd56b546b9d70feaee9
Peter Ndemo <ksamy90@aol.com>
2021-06-04 10:36:49 +0300

    BasicMath

---
+++ Script.js
@@ -0,0 +1,26 @@
+function add(a,b) {
+    return a+b;
+}
+console.log(add(2,4));
+function subtract(a,b) {
+    return a-b;
+}
+console.log(subtract(8,4));
+function multiply(a,b) {
+    return a*b;
+}
+console.log(multiply(2,5));
+function divide(a,b) {
+    return a/b;
+}
+console.log(divide(9,3));
+function operate(operator, a,b) {
+    return operator (a,b);
+    
+}
+console.log(divide(8,4));
+
+
+
+
+
